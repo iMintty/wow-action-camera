4dd2f85f6b47c487ab626af7a53a028424e975f4
Minty <imintty@outlook.com>
2020-06-25 17:59:07 +0100

    initial commit

---
+++ scripts/deploy_development.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g 8.3.0
+
+curlfiles=""
+for file in "/home/travis/build/iMintty/wow-action-camera/.release"/*
+do
+    if [ ${file: -4} == ".zip" ]
+    then
+        curlfiles="$curlfiles -F $(basename $file)=@$file"
+    fi
+done
+
+curl $curlfiles $DISCORD_WEBHOOK
+echo $curlfiles
